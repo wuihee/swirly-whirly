7f954be8c012f43eca7d4c4e44643af5d24a128e
wuihee <wuihee@gmail.com>
2024-06-28 14:35:01 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import "./App.css";
+import Navbar from "./components/navbar";
+
+export default function App() {
+  return <Navbar></Navbar>;
+}
---
+++ src/components/navbar.jsx
@@ -0,0 +1,14 @@
+import "../styles/navbar.css";
+
+export default function Navbar() {
+  return (
+    <div id="navbar">
+      <h1>Swirly Whirly</h1>
+      <ul>
+        <li>About</li>
+        <li>Products</li>
+      </ul>
+      <button>Sign In</button>
+    </div>
+  );
+}
