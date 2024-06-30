62338cc6b68a280734d47e06335d033ad5cf61ec
wuihee <wuihee@gmail.com>
2024-06-30 10:32:05 +0800

    Updated homepage

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,14 +1,33 @@
+import { useState } from "react";
 import "../styles/navbar.css";
 
 export default function Navbar() {
+  let [isOpen, setIsOpen] = useState(false);
+
+  function toggleMenu() {
+    setIsOpen(!isOpen);
+  }
+
   return (
-    <div id="navbar">
-      <h1>Swirly Whirly</h1>
-      <ul>
-        <li>About</li>
-        <li>Products</li>
-      </ul>
-      <button>Sign In</button>
-    </div>
+    <nav>
+      <button onClick={toggleMenu}>click</button>
+      {isOpen && <Menu />}
+    </nav>
+  );
+}
+
+function Menu() {
+  return (
+    <ul>
+      <li>
+        <a href="">About</a>
+      </li>
+      <li>
+        <a href="">Proudcts</a>
+      </li>
+      <li>
+        <a href="">Contact</a>
+      </li>
+    </ul>
   );
 }
