7394aa9f07fe57a0b0aa715d2e0333be3f3c30a4
wuihee <wuihee@gmail.com>
2024-07-14 19:03:13 +0800

    Updated website

--- src/components/SectionHeader.jsx
+++
@@ -1,9 +0,0 @@
-import "../styles/SectionHeader.css";
-
-export default function SectionHeader({ title }) {
-  return (
-    <div className="section-header">
-      <h3>{title.toUpperCase()}</h3>
-    </div>
-  );
-}
